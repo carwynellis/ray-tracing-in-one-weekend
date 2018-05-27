1e1a3da28d87cb2660489500c679337159889792
Carwyn Ellis <carwynellis@gmail.com>
2018-05-27 18:41:04 +0100

    Completed Vec3 implementation with tests
    * some methods have been ommitted but can be implemented later if
    required

--- src/main/scala/uk/carwynellis/raytracing/Vec3.scala
+++ src/main/scala/uk/carwynellis/raytracing/Vec3.scala
@@ -9,21 +9,98 @@ package uk.carwynellis.raytracing
   * @param y
   * @param z
   */
-class Vec3(x: Double, y: Double, z: Double) {
+case class Vec3(x: Double, y: Double, z: Double) {
 
   // Alias the x, y, z values.
   val r: Double = x
   val g: Double = y
   val b: Double = z
 
+  // TODO - some operators including array indexing operators not yet implemented.
+  def unary_-(): Vec3 = Vec3( -x, -y, -z)
+  def unary_+(): Vec3 = this
+
   /**
     * Compute the vector length defined as the square root of x^2 + y^2 + z^2^
     * @return
     */
-  def length = math.sqrt( (x * x) + (y * y) + (z * z))
+  def length: Double  = math.sqrt(squaredLength)
+
+  /**
+    * Computes the sum of each of the x,y,z components squared.
+    *
+    * @return
+    */
+  def squaredLength: Double = (x * x) + (y * y) + (z * z)
+
+  // TODO - stream operators >> and << not yet implemented.
+
+  /**
+    * Generates a new vector with total length 1 maintaining the direction of the original vector.
+    *
+    * Note - this returns a new instance of the vector rather than modifying in place as per the mutable c++
+    *        implementation.
+    */
+  def unitVector: Vec3 = this / length
+
+  def +(that: Vec3) = Vec3(
+    x = this.x + that.x,
+    y = this.y + that.y,
+    z = this.z + that.z
+  )
+
+  def -(that: Vec3) = Vec3(
+    x = this.x - that.x,
+    y = this.y - that.y,
+    z = this.z - that.z
+  )
+
+  def *(that: Vec3) = Vec3(
+    x = this.x * that.x,
+    y = this.y * that.y,
+    z = this.z * that.z
+  )
+
+  def *(n: Double) = Vec3(n * x, n * y, n * z)
+
+  def /(that: Vec3) = Vec3(
+    x = this.x / that.x,
+    y = this.y / that.y,
+    z = this.z / that.z
+  )
+
+  def /(n: Double) = Vec3(x / n, y / n, z / n)
+
+  /**
+    * Computes the dot product of this vector and the specified vector.
+    *
+    * @param that
+    * @return
+    */
+  def dot(that: Vec3): Double = (this.x * that.x) + (this.y * that.y) + (this.z * that.z)
+
+  /**
+    * Computes the cross product of this vector and the specified vector.
+    *
+    * Implementation verified with https://betterexplained.com/articles/cross-product/
+    *
+    * @param that
+    * @return
+    */
+  def cross(that: Vec3): Vec3 = Vec3(
+    x = (this.y * that.z) - (this.z * that.y),
+    y = -((this.x * that.z) - (this.z * that.x)),
+    z = (this.x * that.y) - (this.y * that.x)
+  )
+
+  // TODO - += +* etc... methods have not been implemented.
 
 }
 
 object Vec3 {
-  def apply(x: Double, y: Double, z: Double) = new Vec3(x, y, z)
+
+  implicit class DoubleWithVec3Operators(d: Double) {
+    def *(v: Vec3): Vec3 = v * d
+  }
+
 }
