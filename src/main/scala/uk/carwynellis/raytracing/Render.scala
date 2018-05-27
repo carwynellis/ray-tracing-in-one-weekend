cd9adaaf0d6116d04bc1bc78bfc9f2040ca9d93f
Carwyn Ellis <carwynellis@gmail.com>
2018-05-27 20:57:59 +0100

    Added simple render of sphere / ray intersection

--- src/main/scala/uk/carwynellis/raytracing/Render.scala
+++ src/main/scala/uk/carwynellis/raytracing/Render.scala
@@ -8,18 +8,34 @@ import java.io.{File, PrintWriter}
   */
 object Render extends App {
 
-  val nx = 800
-  val ny = 600
+  val nx = 200
+  val ny = 100
 
   /**
-    * Perform a linear blend from blue to white.
+    * Compute the color for a given ray.
+    *
     * @param r
     * @return
     */
   def color(r: Ray): Vec3 = {
-    val unitDirection = r.direction.unitVector
-    val t = 0.5 * (unitDirection.y + 1)
-    ((1.0 - t) * Vec3(1, 1, 1)) + (t * Vec3(0.5, 0.7, 1))
+    if (hitSphere(Vec3(0, 0, -1), 0.5, r)) {
+      // Ray has intersected sphere so we return red for the current pixel.
+      Vec3(1, 0, 0)
+    }
+    else {
+      val unitDirection = r.direction.unitVector
+      val t = 0.5 * (unitDirection.y + 1)
+      ((1.0 - t) * Vec3(1, 1, 1)) + (t * Vec3(0.5, 0.7, 1))
+    }
+  }
+
+  def hitSphere(centre: Vec3, radius: Double, ray: Ray): Boolean = {
+    val oc = ray.origin - centre
+    val a = ray.direction.dot(ray.direction)
+    val b = 2.0 * oc.dot(ray.direction)
+    val c = oc.dot(oc) - (radius * radius)
+    val discriminant = (b * b) - (4 * a * c)
+    discriminant > 0
   }
 
   val fileName = "image.ppm"
