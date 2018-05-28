640658cf14d0f77f05d7b8d5b8008874860f2abc
Carwyn Ellis <carwynellis@gmail.com>
2018-05-28 21:55:26 +0100

    Added initial dieletric material for glass like objects
    * Note refaction works but reflection needs further work

--- src/main/scala/uk/carwynellis/raytracing/Render.scala
+++ src/main/scala/uk/carwynellis/raytracing/Render.scala
@@ -75,7 +75,7 @@ object Render extends App {
     Sphere(Vec3(0, 0, -1), 0.5, new Lambertian(Vec3(0.8, 0.3, 0.3))),
     Sphere(Vec3(0, -100.5, -1), 100, new Lambertian(Vec3(0.8, 0.8, 0.0))),
     Sphere(Vec3(1, 0, -1), 0.5, new Metal(Vec3(0.8, 0.6, 0.2), 0.3)),
-    Sphere(Vec3(-1, 0, -1), 0.5, new Metal(Vec3(0.8, 0.8, 0.8), 1.0))
+    Sphere(Vec3(-1, 0, -1), 0.5, new Dielectric(1.5))
   ))
 
   // Write PPM data
--- src/main/scala/uk/carwynellis/raytracing/material.scala
+++ src/main/scala/uk/carwynellis/raytracing/material.scala
@@ -25,3 +25,24 @@ class Metal(albedo: Vec3, fuzziness: Double) extends Material(albedo) {
     Ray(record.p, reflected + (fuzziness * Sphere.randomPointInUnitSphere()))
   }
 }
+
+class Dielectric(refractiveIndex: Double) extends Material(Vec3(1,1,1)) {
+
+  override def scatter(rayIn: Ray, record: HitRecord): Ray = {
+    val reflected = Material.reflect(rayIn.direction,record.normal)
+    val (outwardNormal, niOverNt) =
+      if (rayIn.direction.dot(record.normal) > 0) (-record.normal, refractiveIndex)
+      else (record.normal, 1.0 / refractiveIndex)
+    val refracted = refract(rayIn.direction, outwardNormal, niOverNt)
+    if (refracted == rayIn.direction) Ray(record.p, reflected)
+    else Ray(record.p, refracted)
+  }
+
+  private def refract(v: Vec3, n: Vec3, niOverNt: Double): Vec3 = {
+    val unitVectorOfV = v.unitVector
+    val dt = unitVectorOfV.dot(n)
+    val discriminant = 1.0 - (niOverNt * niOverNt * (1 - (dt * dt)))
+    if (discriminant > 0) (niOverNt * (unitVectorOfV - (n * dt))) - (n * math.sqrt(discriminant))
+    else v
+  }
+}
